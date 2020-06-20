228975b87eba3a3794f6d4b732ffcc68eb2a9544
FenQiDian <email@fenqi.moe>
2020-06-20 22:59:48 +0800

    switch to ncollide physics engine

--- plugin/src/logic/engine.rs
+++ plugin/src/logic/engine.rs
@@ -1,9 +1,9 @@
-use crate::cpp::bullet as bt;
 use crate::id::{ObjID, ObjectIDGener};
 use crate::logic::*;
 use crate::state::StatePool;
-use crate::utils::mut_ptr;
+use crate::utils::{mut_ptr, Fixed64, fixed64};
 use failure::{format_err, Error};
+use ncollide3d::pipeline::world::CollisionWorld;
 use std::collections::HashMap;
 use std::mem;
 use std::ptr;
@@ -15,74 +15,25 @@ pub struct LogicEngine {
     id_gener: ObjectIDGener,
     stage: Option<Box<LogicStage>>,
     characters: HashMap<ObjID, Box<LogicCharacter>>,
-    bullet: Bullet,
-}
-
-struct Bullet {
-    config: bt::btDefaultCollisionConfiguration,
-    dispatcher: bt::btCollisionDispatcher,
-    broadphase: bt::btDbvtBroadphase,
-    solver: bt::btSequentialImpulseConstraintSolver,
-    world: bt::btDiscreteDynamicsWorld,
+    world: CollisionWorld<Fixed64, ()>,
 }
 
 impl Drop for LogicEngine {
     fn drop(&mut self) {
-        unsafe {
-            bt::btDiscreteDynamicsWorld_btDiscreteDynamicsWorld_destructor(&mut self.bullet.world);
-            bt::btSequentialImpulseConstraintSolver_btSequentialImpulseConstraintSolver_destructor(
-                &mut self.bullet.solver,
-            );
-            bt::btDbvtBroadphase_btDbvtBroadphase_destructor(&mut self.bullet.broadphase);
-            bt::btCollisionDispatcher_btCollisionDispatcher_destructor(&mut self.bullet.dispatcher);
-            bt::btDefaultCollisionConfiguration_btDefaultCollisionConfiguration_destructor(
-                &mut self.bullet.config,
-            );
-        };
     }
 }
 
 impl LogicEngine {
     pub fn new() -> Box<LogicEngine> {
-        let mut engine: Box<LogicEngine> = Box::new(LogicEngine {
+        return Box::new(LogicEngine {
             id_gener: ObjectIDGener::new(),
             stage: None,
             characters: HashMap::new(),
-            bullet: unsafe { mem::zeroed() },
+            world: CollisionWorld::new(fixed64(0.02)),
         });
-        unsafe {
-            engine.bullet.config = bt::btDefaultCollisionConfiguration::new(
-                &bt::bthNewDefaultCollisionConstructionInfo(),
-            );
-            engine.bullet.dispatcher =
-                bt::btCollisionDispatcher::new(mut_ptr(&mut engine.bullet.config));
-            engine.bullet.broadphase = bt::btDbvtBroadphase::new(ptr::null_mut());
-            engine.bullet.solver = bt::btSequentialImpulseConstraintSolver::new();
-            engine.bullet.world = bt::btDiscreteDynamicsWorld::new(
-                mut_ptr(&mut engine.bullet.dispatcher),
-                mut_ptr(&mut engine.bullet.broadphase),
-                mut_ptr(&mut engine.bullet.solver),
-                mut_ptr(&mut engine.bullet.config),
-            );
-
-            bt::btDiscreteDynamicsWorld_setGravity(
-                mut_ptr(&mut engine.bullet.world),
-                &bt::btVector3::new1(&0.0, &-10.0, &0.0),
-            );
-        };
-        return engine;
     }
 
     pub fn update(&mut self, dura: Duration) -> Result<Box<StatePool>, Error> {
-        unsafe {
-            bt::btDiscreteDynamicsWorld_stepSimulation(
-                mut_ptr(&mut self.bullet.world),
-                dura.as_secs_f32(),
-                60,
-                1.0 / 60.0,
-            );
-        };
-
         let mut state_pool = Box::new(StatePool::new(STATE_POOL_SIZE));
 
         // update stage
@@ -106,12 +57,6 @@ impl LogicEngine {
         if self.stage.is_some() {
             return Err(format_err!("Stage already exists."));
         }
-        unsafe {
-            bt::btDiscreteDynamicsWorld_addRigidBody(
-                mut_ptr(&mut self.bullet.world),
-                &mut stage.bullet.body,
-            )
-        };
         self.stage = Some(stage);
         return Ok(());
     }
@@ -120,12 +65,6 @@ impl LogicEngine {
         &mut self,
         mut chara: Box<LogicCharacter>,
     ) -> Result<(), Error> {
-        unsafe {
-            bt::btDiscreteDynamicsWorld_addRigidBody(
-                mut_ptr(&mut self.bullet.world),
-                &mut chara.bullet.body,
-            )
-        };
         self.characters.insert(chara.obj_id(), chara);
         return Ok(());
     }
--- plugin/src/logic/stage.rs
+++ plugin/src/logic/stage.rs
@@ -1,4 +1,3 @@
-use crate::cpp::bullet as bt;
 use crate::id::{ObjID, TYPE_STAGE};
 use crate::logic::{logic_obj, LogicObj};
 use crate::state::{state_data, StateData, StateLifecycle, StatePool};
@@ -10,50 +9,20 @@ use std::time::Duration;
 
 #[logic_obj(TYPE_STAGE)]
 pub struct LogicStage {
-    pub(super) lifecycle: StateLifecycle,
-    pub(super) bullet: Bullet,
-}
-
-pub struct Bullet {
-    pub(super) shape: bt::btStaticPlaneShape,
-    pub(super) body: bt::btRigidBody,
+    lifecycle: StateLifecycle,
 }
 
 impl Drop for LogicStage {
     fn drop(&mut self) {
-        unsafe {
-            bt::btStaticPlaneShape_btStaticPlaneShape_destructor(mut_ptr(&mut self.bullet.shape));
-            bt::btRigidBody_btRigidBody_destructor(&mut self.bullet.body);
-        };
     }
 }
 
 impl LogicStage {
     pub(super) fn new(obj_id: ObjID) -> Box<LogicStage> {
-        let mut stage: Box<LogicStage> = Box::new(LogicStage {
+        return Box::new(LogicStage {
             sup: Self::new_super(obj_id),
             lifecycle: StateLifecycle::Created,
-            bullet: unsafe { mem::zeroed() },
         });
-
-        unsafe {
-            stage.bullet.shape =
-                bt::btStaticPlaneShape::new(&bt::btVector3::new1(&0.0, &1.0, &0.0), 0.0);
-            bt::btConcaveShape_setMargin(mut_ptr(&mut stage.bullet.shape), 0.04);
-
-            stage.bullet.body = bt::btRigidBody::new1(
-                0.0,
-                ptr::null_mut(),
-                mut_ptr(&mut stage.bullet.shape),
-                &bt::btVector3::new1(&0.0, &0.0, &0.0),
-            );
-
-            let mut transform = bt::btTransform::new();
-            transform.setIdentity();
-            transform.setOrigin(&bt::btVector3::new1(&0.0, &0.0, &0.0));
-            bt::btCollisionObject_setWorldTransform(mut_ptr(&mut stage.bullet.body), &transform);
-        };
-        return stage;
     }
 }
 
