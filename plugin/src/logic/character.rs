9fa0aeceda1af9b59a1c06b311a38e92f8caaf6b
FenQiDian <email@fenqi.moe>
2020-06-20 22:59:48 +0800

    switch to ncollide physics engine

--- plugin/src/logic/character.rs
+++ plugin/src/logic/character.rs
@@ -1,19 +1,19 @@
 use crate::id::{ObjID, TYPE_CHARACTER};
 use crate::logic::{logic_obj, LogicObj};
 use crate::state::{state_data, StateData, StateLifecycle, StatePool};
-use crate::utils::mut_ptr;
+use crate::utils::{Fixed64, fixed64};
 use failure::Error;
 use failure::_core::time::Duration;
-use std::mem;
+use ncollide3d::shape::{ShapeHandle, Capsule};
 
 #[logic_obj(TYPE_CHARACTER)]
 pub struct LogicCharacter {
-    lifecycle: StateLifecycle,
+    pub(crate) lifecycle: StateLifecycle,
+    pub(crate) shape: ShapeHandle<Fixed64>,
 }
 
 impl Drop for LogicCharacter {
-    fn drop(&mut self) {
-    }
+    fn drop(&mut self) {}
 }
 
 impl LogicCharacter {
@@ -21,6 +21,7 @@ impl LogicCharacter {
         return Box::new(LogicCharacter {
             sup: Self::new_super(obj_id),
             lifecycle: StateLifecycle::Created,
+            shape: ShapeHandle::new(Capsule::new(fixed64(0.85), fixed64(0.4))),
         });
     }
 }
@@ -35,8 +36,7 @@ impl LogicObj for LogicCharacter {
 
 #[state_data(TYPE_CHARACTER)]
 #[derive(Debug)]
-pub struct StateCharacter {
-}
+pub struct StateCharacter {}
 
 impl StateData for StateCharacter {}
 
