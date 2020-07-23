c10a284fb1378cbc6e38a9a7a6154fed57fcbf6d
FenQiDian <email@fenqi.moe>
2020-07-23 23:29:09 +0800

    math mod

--- math/src/num.rs
+++ math/src/num.rs
@@ -24,3 +24,27 @@ pub fn fx<N: ToFixed>(num: N) -> Fx {
     fixed.0 = I40F24::from_num(num);
     return fixed;
 }
+
+pub fn fxi32(num: Fx) -> i32 {
+    return num.0.to_num::<i32>();
+}
+
+pub fn fxu32(num: Fx) -> u32 {
+    return num.0.to_num::<u32>();
+}
+
+pub fn fxi64(num: Fx) -> i64 {
+    return num.0.to_num::<i64>();
+}
+
+pub fn fxu64(num: Fx) -> u64 {
+    return num.0.to_num::<u64>();
+}
+
+pub fn fxf32(num: Fx) -> f32 {
+    return num.0.to_num::<f32>();
+}
+
+pub fn fxf64(num: Fx) -> f64 {
+    return num.0.to_num::<f64>();
+}
---
+++ math/src/types.rs
@@ -0,0 +1,4 @@
+// re-export nalgebra types to resolve godot name confilicts
+
+pub use na::base::Vector3;
+pub use na::geometry::{Point3, Rotation3, Translation3};
