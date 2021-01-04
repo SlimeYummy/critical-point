355d9652085ae9197c03080b79be64d3ca68883f
FenQiDian <email@fenqi.moe>
2021-01-04 21:05:08 +0800

    build script

--- script/build.bat
+++ script/build.bat
@@ -1,8 +1,10 @@
 @echo off
 if "%1" == "--release" (
     cargo build --release
-    copy .\target\release\critical_point_gd.dll .\scene
+    copy .\target\release\critical_point_gd.dll .\scene\native
+    .\target\release\compiler.exe .\scene\critical_point\resource.yml .\scene\critical_point\id.yml
 ) else (
     cargo build
-    copy .\target\debug\critical_point_gd.dll .\scene
+    copy .\target\debug\critical_point_gd.dll .\scene\native
+    .\target\debug\compiler.exe .\scene\critical_point\resource.yml .\scene\critical_point\id.yml
 )
--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,9 @@
 if [ "$1" = "--release" ]; then
     cargo build --release
-    cp ./target/release/critical_point_gd.dll ./scene
+    cp ./target/release/critical_point_gd.dll ./scene/native
+    ./target/release/compiler ./scene/critical_point/resource.yml ./scene/critical_point/id.yml
 else
     cargo build
-    cp ./target/debug/critical_point_gd.dll ./scene
+    cp ./target/debug/critical_point_gd.dll ./scene/native
+    ./target/debug/compiler ./scene/critical_point/resource.yml ./scene/critical_point/id.yml
 fi
