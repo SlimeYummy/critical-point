76add3d5aab2dc6369b81ef687ddcc514600f8da
FenQiDian <email@fenqi.moe>
2021-07-14 20:07:00 +0800

    compiler

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -3,18 +3,23 @@ extern crate core;
 use core::resource::ResCache;
 use core::utils::serialize;
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
-    if env::args().len() != 3 {
-        println!("Usage: compiler <resource.yaml>");
+    if env::args().len() != 4 {
+        println!("Usage: compiler <./root/path> <resource.yml> <id.yml>");
         return;
     }
-    let res_file = env::args().nth(1).unwrap();
-    let id_file = env::args().nth(2).unwrap();
+    let root_path = env::args().nth(1).unwrap();
+    let res_file = env::args().nth(2).unwrap();
+    let id_file = env::args().nth(3).unwrap();
 
-    let cache = ResCache::compile(&res_file).unwrap();
+    let cache = ResCache::compile(&root_path, &res_file).unwrap();
     let table = cache.id_table();
-    serialize(id_file, table).unwrap();
+
+    let mut id_path = PathBuf::from(root_path);
+    id_path.push(id_file);
+    serialize(id_path, table).unwrap();
 
     println!("Compile resource success");
 }
