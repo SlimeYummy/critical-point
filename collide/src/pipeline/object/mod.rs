1b6d881f2de676dd02a90f0ea77b15d1a13c0490
FenQiDian <email@fenqi.moe>
2021-08-17 23:32:58 +0800

    simplify collision group

--- collide/src/pipeline/object/mod.rs
+++ collide/src/pipeline/object/mod.rs
@@ -2,7 +2,7 @@ mod collision_groups;
 mod collision_object;
 mod collision_object_set;
 
-pub use self::collision_groups::{CollisionGroups, CollisionTeam, CollisionTeamFilter};
+pub use self::collision_groups::CollisionGroups;
 pub use self::collision_object::{CollisionObject, CollisionObjectType};
 pub use self::collision_object_set::{CollisionObjectSlab, CollisionObjects};
 pub use ncollide3d::pipeline::{
