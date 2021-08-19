d728bad6d216061ee0bfd2857645bb7d0cc43d57
FenQiDian <email@fenqi.moe>
2021-08-19 21:50:52 +0800

    aligned size of

--- core/src/utils/mod.rs
+++ core/src/utils/mod.rs
@@ -5,6 +5,6 @@ mod rc_cell;
 pub mod serde_helper;
 mod serialize;
 
-pub use ptr::{const_ptr, mut_ptr, CastArc, CastRc};
+pub use ptr::{const_ptr, mut_ptr, size_of_array, size_of_type, CastArc, CastRc};
 pub use rc_cell::{RcCell, RcCellError, RcCellRef, RcCellRefMut};
 pub use serialize::{deserialize, serialize};
